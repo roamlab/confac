5828349b92ecbb35191fff994fd2abfc804dd83f
Gagan Khandate <gagank@cs.columbia.edu>
2020-03-18 13:58:08 -0400

    Moved get_section to dump_section, new method get_section, getint

--- confac/configparser.py
+++ confac/configparser.py
@@ -10,7 +10,7 @@ class ConfigParser(configparser.ConfigParser):
     def __init__(self):
         super().__init__()
 
-    def save(self, path: str):
+    def save(self, path):
 
         """
         Save config to path
@@ -26,7 +26,40 @@ class ConfigParser(configparser.ConfigParser):
         with open(path, 'w') as f:
             self.write(f)
 
-    def get_section(self, section: str, recursive=False, dump=None):
+    def get_section(self, section, options):
+
+        """
+        Equivalent of get but for section
+
+        Args:
+            section(str): section name
+            options(dict): dict of options and type
+
+        Returns:
+            dict of options with corresponding parsed values
+
+        """
+        assert self.has_section(section), 'section {} not found'.format(section)
+        assert isinstance(options, dict), 'options must be a dict'
+
+        sec = {}
+        for opt, typ in options.items():
+            if self.has_option(sec, opt):
+                if typ == 'bool':
+                    sec[opt] = self.getboolean(section, opt)
+                elif typ == 'int':
+                    sec[opt] = self.getint(section, opt)
+                elif typ == 'float':
+                    sec[opt] = self.getfloat(section, opt)
+                elif typ == 'list':
+                    sec[opt] = self.getlist(section, opt)
+                elif typ == 'eval':
+                    sec[opt] = eval(self.get(section, opt))
+                elif typ == 'str':
+                    sec[opt] = self.get(section, opt)
+        return sec
+
+    def dump_section(self, section, recursive=False, dump=None):
 
         """
         Get section and copy into dump, recurse if required
@@ -47,11 +80,11 @@ class ConfigParser(configparser.ConfigParser):
             for opt, val in self.items(section):
                 dump.set(section, opt, val)
                 if recursive:
-                    dump = self.get_section(val, recursive=recursive, dump=dump)
+                    dump = self.dump_section(val, recursive=recursive, dump=dump)
         else:
             return dump
 
-    def rename_section(self, old: str, new: str):
+    def rename_section(self, old, new):
 
         """ Renames section """
 
@@ -61,7 +94,6 @@ class ConfigParser(configparser.ConfigParser):
             raise ValueError("section {} already exists".format(new))
 
         self.add_section(new)
-
         for opt, val in self.items(old):
             self.set(new, opt, val)
 
@@ -69,3 +101,6 @@ class ConfigParser(configparser.ConfigParser):
 
     def getlist(self, section, option):
         return ast.literal_eval(self.getlist(section, option))
+
+    def getint(self, *args, **kwargs):
+        return int(super().getfloat(*args, **kwargs))
